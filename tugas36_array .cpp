3061aecb06c60fa48505532008f777f025cfd99a
Rifqi-Irfansyah <rifqiirfansyah518@gmail.com>
2023-10-02 21:33:52 +0700

    feat: add task array

---
+++ tugas36_array .cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, i;
+    float x[6], pembilang, penyebut, jmlh;
+
+    jmlh = 0;
+    pembilang = 0;
+    penyebut = 0;
+    a = 2;
+    b = 9;
+    i = 1;
+    while (i <= 5)
+    {
+        cout << "Masukkan Bilangan [" << i << "] = ";
+        cin >> x[i];
+        pembilang = pembilang + (a / x[i]) ;
+        penyebut = penyebut + (b * x[i]);
+        a = a + 2;
+        b = b - 2;
+        i = i + 1;
+    }
+    jmlh = pembilang / penyebut;
+    cout << "Z = " << jmlh ;
+    return 0;
+}
