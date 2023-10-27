0d4d91f7b60aab1e6497fa58b88005bd52575192
Rifqi Irfansyah <rifqiirfansyah518@gmail.com>
2023-10-27 09:23:16 +0700

    modified: accumulate array 2 dimension

--- tugas42_array 2 dimensi.cpp
+++ tugas42_array 2 dimensi.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 
 int main(){
-    int A[4][4], i, j;
-
+    int A[4][4], jumlah, i, j;
+    jumlah = 0;
     i = 1;
     while(i<=3){
         j = 1;
         while(j<=3){
             cout << "A [" << i << "][" << j << "] = ";
             cin >> A[i][j];
-            j = j + 1;
+            j= j + 1;
         }
         i = i + 1;
     }
-    cout << "-----------------------------" <<endl;
     i = 1;
     while(i<=3){
         j = 1;
         while(j<=3){
-            cout << "A [" << i << "][" << j << "] = " << A[i][j]  <<endl;
+            jumlah = jumlah + A[i][j];
             j = j + 1;
         }
         i = i + 1;
     }
+    cout << "Hasil Penjumlahan Array = " << jumlah;
     return 0;
 }
